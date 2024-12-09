eee37a5122c8bf7858d9bb2a57fdf47ab1f37cb1
angelajarae <angelajaraespinoza@gmail.com>
2024-12-09 02:35:21 -0500

    argument passing

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -38,8 +38,12 @@ process_execute (const char *file_name)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
 
-  /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+   /* Only assign argv[0] to thread's name */
+  char *cmd_name, *args;
+  cmd_name = strtok_r (fn_copy, " ", &args);
+
+  tid = thread_create (cmd_name, PRI_DEFAULT, start_process, args);
+
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
